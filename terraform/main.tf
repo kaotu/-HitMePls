3b10c7f061c30f7db3d93ee66e193ffa6bb4c156
DESKTOP-64B8217\kao-tu <patthraporn.tu@mail.kmutt.ac.th>
2019-06-02 13:09:16 +0700

    [ADD] script ansible

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ provider "google" {
 resource "google_compute_instance" "tf_instance" {
   name         = "tf-instance-${count.index}"
   machine_type = "n1-standard-1"
-  count        = 3
+  count        =  1
 
   boot_disk {
     initialize_params {
